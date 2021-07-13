0ecbe38f26c90c0c5e81739e37751f1acac987b0
acosic3 <antonijo.cosic@fina.hr>
2021-07-13 10:35:53 +0200

    upload projekta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-angular-news-bar';
+  title = 'angular-news-bar';
 }
