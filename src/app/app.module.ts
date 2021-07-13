ed6187a651d213548479746ef64865b58b627ace
acosic3 <antonijo.cosic@fina.hr>
2021-07-13 14:33:12 +0200

    Prijava component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrijavaComponent } from './prijava/prijava.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrijavaComponent
   ],
   imports: [
     BrowserModule,
